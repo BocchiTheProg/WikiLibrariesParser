dcbda484da9c4a0bff38e36da78a1618f2416966
BocchiTheProg <bocchitheprog@gmail.com>
2023-11-12 00:25:26 +0200

    Parsing libraries names, output in console.

--- parser.rb
+++ parser.rb
@@ -1,1 +1,22 @@
+require 'nokogiri'
+require 'open-uri'
+
 puts "Hello Nokogiri parser"
+libraries = []
+url = 'https://en.wikipedia.org/wiki/List_of_Libraries'
+doc = Nokogiri::HTML(URI.open(url))
+doc.css('div#bodyContent h3').each do |alphabet_header|
+  element = alphabet_header.next_element
+  loop do
+    break if element.name == 'h3' or element.name == 'h2'
+    if element.name == 'ul'
+      element.css('li').each do |library|
+        libraries << library.text.strip
+      end
+    end
+    element = element.next_element
+  end
+end
+
+puts libraries.size
+puts libraries
